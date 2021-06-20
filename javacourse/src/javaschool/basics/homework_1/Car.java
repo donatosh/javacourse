2248f5f2cfaa1a9c4ace71215330ed0701ad3a77
Donat Oshchepkov <donat.oshchepkov@gmail.com>
2021-06-20 23:55:38 +0300

    Create project

---
+++ javacourse/src/javaschool/basics/homework_1/Car.java
@@ -0,0 +1,8 @@
+package javaschool.basics.homework_1;
+import javaschool.basics.homework_1.lib.*;
+
+public class Car extends Transport {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ javacourse/src/javaschool/basics/homework_1/lib/Transport.java
@@ -0,0 +1,8 @@
+package javaschool.basics.homework_1.lib;
+
+
+
+public abstract class Transport {
+    private int a;
+
+}
